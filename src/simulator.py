2fa2cc6abb16e96ffa24837be7209cbd4810b082
Marc Schouler <schouler.marc@gmail.com>
2024-04-12 12:52:49 +0200

    FEAT - track gen/cand with dict structure and add postprocessing method

--- src/simulator.py
+++ src/simulator.py
@@ -1,4 +1,5 @@
 import os
+import pandas as pd
 import shutil
 import subprocess
 
@@ -23,6 +24,9 @@ class Simulator(ABC):
         self.exec_cmd: list[str] = config["simulator"]["exec_cmd"].split(" ")
         self.ref_input: str = config["simulator"]["ref_input"]
         self.sim_args: dict = config["simulator"].get("sim_args", {})
+        self.post_process_args: dict = config["simulator"].get("post_process", {})
+        # simulation results
+        self.df_list: list[pd.DataFrame] = []
 
     def custom_input(self, fname: str):
         """
@@ -66,7 +70,7 @@ class WolfSimulator(Simulator):
     """
     def __init__(self, config: dict, meshfile: str = ""):
         super().__init__(config, meshfile)
-        self.wolf_pro: list[tuple[str, subprocess.Popen[str]]] = []
+        self.wolf_pro: list[tuple[dict, subprocess.Popen[str]]] = []
 
     def process_config(self):
         """
@@ -78,13 +82,21 @@ class WolfSimulator(Simulator):
             raise Exception(f"ERROR -- no <ref_input> entry in {self.config}[simulator]")
         if "sim_args" not in self.config["simulator"]:
             print(f"WARNING -- no <sim_args> entry in {self.config}[simulator]")
+        if "post_process" not in self.config["simulator"]:
+            print(f"WARNING -- no <post_process> entry in {self.config}[simulator]")
+
+    def get_sim_outdir(self, gid: int = 0, cid: int = 0) -> str:
+        """
+        Return the path to the folder containing the simulation results.
+        """
+        return os.path.join(self.outdir, f"wolf_g{gid}_c{cid}")
 
     def execute_sim(self, gid: int = 0, cid: int = 0):
         """
         Run a Wolf simulation.
         """
         # generate custom input by altering the wolf template
-        sim_outdir = os.path.join(self.outdir, f"wolf_g{gid}_c{cid}")
+        sim_outdir = self.get_sim_outdir()
         check_dir(sim_outdir)
         self.custom_input(os.path.join(sim_outdir, f"{self.meshfile[:-5]}.wolf"))
         # copy meshfile to the output directory
@@ -106,22 +118,46 @@ class WolfSimulator(Simulator):
                                         universal_newlines=True)
         os.chdir(self.cwd)
         # append simulation to the list of active processes
-        self.wolf_pro.append((f"g{gid}_c{cid}", proc))
+        self.wolf_pro.append(({"generation": gid, "candidate": cid}, proc))
 
     def monitor_sim_progress(self) -> int:
         """
         Update the list of simulations under execution and return their number.
         """
         finished_sim = []
-        for id, (str_id, p_id) in enumerate(self.wolf_pro):
+        for id, (dict_id, p_id) in enumerate(self.wolf_pro):
             returncode = p_id.poll()
             if returncode is None:
                 pass  # simulation still running
             elif returncode == 0:
-                print(f">> simulation {str_id} finished")
+                print(f">> simulation {dict_id} finished")
                 finished_sim.append(id)
+                self.df_list.append(self.post_process(dict_id))
                 break
             else:
                 raise Exception(f"ERROR -- simulation {p_id} crashed")
         self.wolf_pro = [tup for id, tup in enumerate(self.wolf_pro) if id not in finished_sim]
         return len(self.wolf_pro)
+
+    def post_process(self, dict_id: dict) -> pd.DataFrame:
+        """
+        Post-process the results of a terminated simulation.
+        """
+        sim_out_dir = self.get_sim_outdir(dict_id["generation"], dict_id["candidate"])
+        qty_list: list[list[float]] = []
+        head_list: list[str] = []
+        for key, value in self.post_process_args.items():
+            file = open(os.path.join(sim_out_dir, key), "r").read().splitlines()
+            headers = file[0][2:].split()  # ignore "# " before "Iter" in headers
+            for qty in value:
+                try:
+                    idx = headers.index(qty)
+                    qty_list.append([float(line.split()[idx]) for line in file[1:]])
+                    head_list.append(qty)
+                except Exception as e:
+                    print(f"WARNING -- could not read {qty} in {headers}")
+                    print(f"WARNING -- exception {e} was raised")
+        df = pd.DataFrame({head_list[i]: qty_list[i] for i in range(len(qty_list))})
+        print(f">> g{dict_id['generation']}, c{dict_id['candidate']} converged in {len(df)}")
+        print(f">> last five values: {df.tail(n=5).to_string(index=False)}")
+        return df
