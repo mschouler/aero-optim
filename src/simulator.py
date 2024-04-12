2e54713226e7f158708968642ac150e54422ca9a
Marc Schouler <schouler.marc@gmail.com>
2024-04-12 19:44:27 +0200

    FIX/FEAT - improve comments and enable multi-simulation execution

--- src/simulator.py
+++ src/simulator.py
@@ -9,18 +9,24 @@ from .utils import check_dir
 
 class Simulator(ABC):
     """
-    This class implements a basic Simulator.
+    This class implements a basic Simulator class.
     """
-    def __init__(self, config: dict, meshfile: str = ""):
+    def __init__(self, config: dict):
+        """
+        Instantiates the Simulator object.
+        >> cwd: the working directory.
+        >> outdir: the output directory where the simulation results folder will be stored.
+        >> ref_input: a simulation input file template.
+        >> sim_args: arguments to modify to customize ref_input.
+        >> post_process_args: quantities to extract from result files.
+        >> def_list: list of dataframe containinf the extracted quantities of all simulation.
+        """
         self.cwd: str = os.getcwd()
         self.config = config
         self.process_config()
         # study params
         self.outdir: str = config["study"]["outdir"]
         # simulator params
-        full_meshfile: str = config["simulator"]["file"] if not meshfile else meshfile
-        self.path_to_meshfile: str = "/".join(full_meshfile.split("/")[:-1])
-        self.meshfile: str = full_meshfile.split("/")[-1]
         self.exec_cmd: list[str] = config["simulator"]["exec_cmd"].split(" ")
         self.ref_input: str = config["simulator"]["ref_input"]
         self.sim_args: dict = config["simulator"].get("sim_args", {})
@@ -30,7 +36,7 @@ class Simulator(ABC):
 
     def custom_input(self, fname: str):
         """
-        Use the reference input to generate a new customized one.
+        Modifies the reference input to generate a new customized one.
         """
         ref_output = open(self.ref_input, "r").read().splitlines()
         for key, value in self.sim_args.items():
@@ -54,62 +60,75 @@ class Simulator(ABC):
     @abstractmethod
     def process_config(self):
         """
-        Make sure the config file contains the required information and extract it.
+        Makes sure the config file contains the required information.
         """
 
     @abstractmethod
-    def execute_sim(self, gid: int = 0, cid: int = 0):
+    def execute_sim(self, meshfile: str = "", gid: int = 0, cid: int = 0):
         """
-        Run a single simulation.
+        Runs a single simulation.
         """
 
 
 class WolfSimulator(Simulator):
     """
-    This class implements a Wolf Simulator.
+    This class implements a WolfSimulator class.
     """
-    def __init__(self, config: dict, meshfile: str = ""):
-        super().__init__(config, meshfile)
+    def __init__(self, config: dict):
+        """
+        Instantiates the WolfSimulator object.
+        >> wolf_pro: list to track simulations and their associated subprocess.
+           It has the following form ({'generation': gid, 'candidate': cid}, subprocess).
+        """
+        super().__init__(config)
         self.wolf_pro: list[tuple[dict, subprocess.Popen[str]]] = []
 
     def process_config(self):
         """
-        Make sure the config file contains the required information and extract it.
+        Makes sure the config file contains the required information and extract it.
         """
         if "exec_cmd" not in self.config["simulator"]:
-            raise Exception(f"ERROR -- no <exec_cmd> entry in {self.config}[simulator]")
+            raise Exception(f"ERROR -- no <exec_cmd> entry in {self.config['simulator']}")
         if "ref_input" not in self.config["simulator"]:
-            raise Exception(f"ERROR -- no <ref_input> entry in {self.config}[simulator]")
+            raise Exception(f"ERROR -- no <ref_input> entry in {self.config['simulator']}")
         if "sim_args" not in self.config["simulator"]:
-            print(f"WARNING -- no <sim_args> entry in {self.config}[simulator]")
+            print(f"WARNING -- no <sim_args> entry in {self.config['simulator']}")
         if "post_process" not in self.config["simulator"]:
-            print(f"WARNING -- no <post_process> entry in {self.config}[simulator]")
+            print(f"WARNING -- no <post_process> entry in {self.config['simulator']}")
 
     def get_sim_outdir(self, gid: int = 0, cid: int = 0) -> str:
         """
-        Return the path to the folder containing the simulation results.
+        Returns the path to the folder containing the simulation results.
         """
         return os.path.join(self.outdir, f"wolf_g{gid}_c{cid}")
 
-    def execute_sim(self, gid: int = 0, cid: int = 0):
+    def execute_sim(self, meshfile: str = "", gid: int = 0, cid: int = 0):
         """
-        Run a Wolf simulation.
+        Pre-processes and executes a Wolf simulation.
+        It also updates wolf_pro.
         """
+        # Pre-process
+        # get the simulation meshfile
+        full_meshfile = meshfile if meshfile else self.config["simulator"]["file"]
+        path_to_meshfile: str = "/".join(full_meshfile.split("/")[:-1])
+        meshfile = full_meshfile.split("/")[-1]
         # generate custom input by altering the wolf template
-        sim_outdir = self.get_sim_outdir()
+        sim_outdir = self.get_sim_outdir(gid=gid, cid=cid)
         check_dir(sim_outdir)
-        self.custom_input(os.path.join(sim_outdir, f"{self.meshfile[:-5]}.wolf"))
+        self.custom_input(os.path.join(sim_outdir, f"{meshfile[:-5]}.wolf"))
         # copy meshfile to the output directory
-        shutil.copy(os.path.join(self.path_to_meshfile, self.meshfile), sim_outdir)
-        print(f">> {os.path.join(self.path_to_meshfile, self.meshfile)} copied to {sim_outdir}")
+        shutil.copy(os.path.join(path_to_meshfile, meshfile), sim_outdir)
+        print(f">> {os.path.join(path_to_meshfile, meshfile)} copied to {sim_outdir}")
         # update the execution command with the right mesh file
         exec_cmd = self.exec_cmd.copy()
         idx = self.exec_cmd.index("@.mesh")
-        exec_cmd[idx] = os.path.join(self.meshfile)
+        exec_cmd[idx] = os.path.join(meshfile)
+        # Execution
         # move to the output directory, execute wolf and move back to the main directory
         os.chdir(sim_outdir)
         with open(f"wolf_g{gid}_c{cid}.out", "wb") as out:
             with open(f"wolf_g{gid}_c{cid}.err", "wb") as err:
+                print(f">> execute simulation g{gid} c{cid} with Wolf")
                 proc = subprocess.Popen(exec_cmd,
                                         env=os.environ,
                                         stdin=subprocess.DEVNULL,
@@ -122,7 +141,7 @@ class WolfSimulator(Simulator):
 
     def monitor_sim_progress(self) -> int:
         """
-        Update the list of simulations under execution and return their number.
+        Updates and returns the list of simulations under execution.
         """
         finished_sim = []
         for id, (dict_id, p_id) in enumerate(self.wolf_pro):
@@ -142,6 +161,7 @@ class WolfSimulator(Simulator):
     def post_process(self, dict_id: dict) -> pd.DataFrame:
         """
         Post-process the results of a terminated simulation.
+        Returns the extracted results in a DataFrame.
         """
         sim_out_dir = self.get_sim_outdir(dict_id["generation"], dict_id["candidate"])
         qty_list: list[list[float]] = []
@@ -158,6 +178,12 @@ class WolfSimulator(Simulator):
                     print(f"WARNING -- could not read {qty} in {headers}")
                     print(f"WARNING -- exception {e} was raised")
         df = pd.DataFrame({head_list[i]: qty_list[i] for i in range(len(qty_list))})
-        print(f">> g{dict_id['generation']}, c{dict_id['candidate']} converged in {len(df)}")
+        print(f">> g{dict_id['generation']}, c{dict_id['candidate']} converged in {len(df)} it.")
         print(f">> last five values: {df.tail(n=5).to_string(index=False)}")
         return df
+
+    def kill_all(self):
+        """
+        Kills all active processes.
+        """
+        _ = [subpro.terminate() for _, subpro in self.wolf_pro]
