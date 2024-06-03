5d588f2b90310d68518d0c19206e5fb20cb6a4ac
Marc Schouler <schouler.marc@gmail.com>
2024-06-03 16:52:12 +0200

    remove duplicated code

--- src/optim/optimizer.py
+++ src/optim/optimizer.py
@@ -1,8 +1,13 @@
 import logging
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 import numpy as np
 import os
+import signal
+import time
 
 from abc import ABC, abstractmethod
+from inspyred.ec import Individual
 from random import Random
 from typing import Any
 
@@ -72,11 +77,18 @@ class Optimizer(ABC):
         - gen_ctr (int): generation counter.
         - generator (Generator): Generator object for the initial generation sampling.
         - ffd (FFD_2D): FFD_2D object to generate deformed geometries.
-        - gmsh_mesh (Mesh): Mesh object to generate deformed geometries meshes.
+        - gmsh_mesh (Mesh): Mesh class to generate deformed geometries meshes.
+        - simulator (Simulator): Simulator object to perform simulations.
         - mean (list[float]): list of populations mean fitness.
         - median (list[float]): list of populations median fitness.
         - max (list[float]): list of populations max fitness.
         - min (list[float]): list of populations min fitness.
+        - J (list[float]): the list of all generated candidates fitnesses.
+        - ffd_profiles (list[list[np.ndarray]]): all deformed geometries {gid: {cid: ffd_profile}}.
+        - QoI (str): the quantity of intereset to minimize/maximize.
+        - n_plt (int): the number of best candidates results to display after each evaluation.
+        - cmap (str): the colormaps used for the observer plot</br>
+            see https://matplotlib.org/stable/users/explain/colors/colormaps.html.
         """
         self.config = config
         self.process_config()
@@ -110,11 +122,19 @@ class Optimizer(ABC):
             self.ffd: FFD_2D = FFD_2D(self.dat_file, self.n_design // 2)
             self.set_gmsh_mesh()
         self.set_simulator()
+        self.simulator = self.SimulatorClass(self.config)
         # population statistics
         self.mean: list[float] = []
         self.median: list[float] = []
         self.max: list[float] = []
         self.min: list[float] = []
+        # set other inner optimization variables
+        self.J: list[float] = []
+        self.ffd_profiles: list[list[np.ndarray]] = []
+        self.QoI: str = self.config["optim"].get("QoI", "CD")
+        self.n_plt: int = self.config["optim"].get("n_plt", 5)
+        self.cmap: str = self.config["optim"].get("cmap", "viridis")
+        self.set_inner()
 
     def process_config(self):
         """
@@ -167,6 +187,24 @@ class Optimizer(ABC):
             else:
                 raise Exception(f"ERROR -- incorrect study_type <{self.study_type}>")
 
+    def set_inner(self):
+        """
+        **Sets** some use-case specific inner variables:
+
+        - baseline_CD (float): the drag coefficient of the baseline geometry.
+        - baseline_CL (float): the lift coefficient of the baseline geometry.
+        - baseline_area (float): the baseline area that is used as a structural constraint.
+        - area_margin (float): area tolerance margin given as a percentage wrt baseline_area</br>
+            i.e. a candidate with an area greater/smaller than +/- area_margin % of the
+            baseline_area will be penalized.
+        - penalty (list): a [key, value] constraint not to be worsen by the optimization.
+        """
+        self.baseline_CD: float = self.config["optim"].get("baseline_CD", 0.15)
+        self.baseline_CL: float = self.config["optim"].get("baseline_CL", 0.36)
+        self.baseline_area: float = shoe_lace(self.ffd.pts)
+        self.area_margin: float = self.config["optim"].get("area_margin", 40.) / 100.
+        self.penalty: list = self.config["optim"].get("penalty", ["CL", self.baseline_CL])
+
     def deform(self, Delta: np.ndarray, gid: int, cid: int) -> tuple[str, np.ndarray]:
         """
         **Applies** FFD on a given candidate and returns its resulting file.
@@ -187,10 +225,144 @@ class Optimizer(ABC):
         gmsh_mesh.build_mesh()
         return gmsh_mesh.write_mesh(mesh_dir)
 
+    def execute_candidates(self, candidates: list[Individual] | np.ndarray, gid: int):
+        """
+        **Executes** all candidates and **waits** for them to finish.
+        """
+        logger.info(f"evaluating candidates of generation {self.gen_ctr}..")
+        self.ffd_profiles.append([])
+        for cid, cand in enumerate(candidates):
+            ffd_file, ffd_profile = self.deform(cand, gid, cid)
+            self.ffd_profiles[gid].append(ffd_profile)
+            # meshing with proper sigint management
+            # see https://gitlab.onelab.info/gmsh/gmsh/-/issues/842
+            ORIGINAL_SIGINT_HANDLER = signal.signal(signal.SIGINT, signal.SIG_DFL)
+            mesh_file = self.mesh(ffd_file)
+            signal.signal(signal.SIGINT, ORIGINAL_SIGINT_HANDLER)
+            while self.simulator.monitor_sim_progress() * self.nproc_per_sim >= self.budget:
+                time.sleep(1)
+            self.simulator.execute_sim(meshfile=mesh_file, gid=gid, cid=cid)
+
+        # wait for last candidates to finish
+        while self.simulator.monitor_sim_progress() > 0:
+            time.sleep(1)
+
+    def compute_statistics(self, gen_fitness: np.ndarray):
+        """
+        **Computes** generation statistics.
+        """
+        self.mean.append(np.mean(gen_fitness))
+        self.median.append(np.median(gen_fitness))
+        self.min.append(min(gen_fitness))
+        self.max.append(max(gen_fitness))
+
     def _observe(self, *args, **kwargs):
         """
         Plot evolution after each evaluation.
         """
+
+    def plot_generation(
+            self,
+            gid: int,
+            sorted_idx: np.ndarray,
+            gen_fitness: np.ndarray,
+            fig_name: str
+    ):
+        """
+        **Plots** and **saves** the results of the last evaluated generation.
+        """
+        baseline: np.ndarray = self.ffd.pts
+        profiles: list[np.ndarray] = self.ffd_profiles[gid]
+        res_dict = self.simulator.df_dict[gid]
+        df_key = res_dict[0].columns  # "ResTot", "CD", "CL", "ResCD", "ResCL", "x", "y", "Cp"
+
+        cmap = mpl.colormaps[self.cmap].resampled(self.n_plt)
+        colors = cmap(np.linspace(0, 1, self.n_plt))
+        # subplot construction
+        fig = plt.figure(figsize=(16, 12))
+        ax1 = plt.subplot(2, 1, 1)  # profiles
+        ax2 = plt.subplot(2, 3, 4)  # ResTot
+        ax3 = plt.subplot(2, 3, 5)  # CD & CL
+        ax4 = plt.subplot(2, 3, 6)  # fitness (CD)
+        plt.subplots_adjust(wspace=0.25)
+        ax1.plot(baseline[:, 0], baseline[:, 1], color="k", lw=2, ls="--", label="baseline")
+        ax3.axhline(y=self.baseline_CD, color='k', label="baseline")
+        ax3.axhline(y=self.baseline_CL, color='k', linestyle="--", label="baseline")
+        ax4.axhline(y=self.baseline_CD, color='k', linestyle="--", label="baseline")
+        # loop over candidates through the last generated profiles
+        for color, cid in enumerate(sorted_idx):
+            ax1.plot(profiles[cid][:, 0], profiles[cid][:, 1], color=colors[color], label=f"c{cid}")
+            res_dict[cid][df_key[0]].plot(ax=ax2, color=colors[color], label=f"c{cid}")  # ResTot
+            res_dict[cid][df_key[1]].plot(ax=ax3, color=colors[color], label=f"{df_key[1]} c{cid}")
+            res_dict[cid][df_key[2]].plot(
+                ax=ax3, color=colors[color], ls="--", label=f"{df_key[2]} c{cid}"
+            )
+            ax4.scatter(cid, gen_fitness[cid], color=colors[color], label=f"c{cid}")
+        # legend and title
+        fig.suptitle(
+            f"Generation {gid} - {self.n_plt} top candidates", size="x-large", weight="bold", y=0.93
+        )
+        # top
+        ax1.set_title("FFD profiles", weight="bold")
+        ax1.legend(loc="center left", bbox_to_anchor=(1, 0.5))
+        ax1.set_xlabel('x')
+        ax1.set_ylabel('y')
+        # bottom left
+        ax2.set_title(f"{df_key[0]}", weight="bold")
+        ax2.set_yscale("log")
+        ax2.set_xlabel('it. #')
+        ax2.set_ylabel('residual')
+        # bottom center
+        ax3.set_title(f"{df_key[1]} & {df_key[2]}", weight="bold")
+        ax3.set_xlabel('it. #')
+        ax3.set_ylabel('aerodynamic coeff.')
+        # bottom right
+        ax4.set_title(f"fitness: penalized {self.QoI}", weight="bold")
+        ax4.legend(loc="center left", bbox_to_anchor=(1, 0.5))
+        ax4.set_xlabel('candidate #')
+        ax4.set_ylabel("fitness")
+        # save figure as png
+        logger.info(f"saving {fig_name} to {self.outdir}")
+        plt.savefig(os.path.join(self.outdir, fig_name), bbox_inches='tight')
+        plt.close()
+
+    def plot_progress(self, gen_nbr: int, fig_name: str, baseline_value: float | None = None):
+        """
+        **Plots** and **saves** the overall progress of the optimization.
+        """
+        logger.info(f"plotting populations statistics after {gen_nbr} generations..")
+
+        # plot construction
+        _, ax = plt.subplots(figsize=(8, 8))
+        psize = self.doe_size
+        if baseline_value:
+            ax.axhline(y=baseline_value, color='k', ls="--", label="baseline")
+
+        # plotting data
+        best = self.max if self.maximize else self.min
+        worst = self.min if self.maximize else self.max
+        data = [self.mean, self.median, best, worst]
+        colors = ["grey", "blue", "green", "red"]
+        labels = ["mean", "median", "best", "worst"]
+        for val, col, lab in zip(data, colors, labels):
+            ax.plot(range(self.gen_ctr), val, color=col, label=lab)
+        plt.fill_between(range(self.gen_ctr), data[2], data[3], color='#e6f2e6')
+        plt.grid(True)
+        ymin = min([min(d) for d in data])
+        ymax = max([max(d) for d in data])
+        yrange = ymax - ymin
+        plt.ylim((ymin - 0.1 * yrange, ymax + 0.1 * yrange))
+
+        # legend and title
+        ax.set_title(f"Optimization evolution ({gen_nbr} g. x {psize} c.)")
+        ax.legend(loc="center left", bbox_to_anchor=(1, 0.5))
+        ax.set_xlabel('generation #')
+        ax.set_ylabel('penalized fitness')
+
+        # save figure as png
+        logger.info(f"saving {fig_name} to {self.outdir}")
+        plt.savefig(os.path.join(self.outdir, fig_name), bbox_inches='tight')
+        plt.close()
 
     @abstractmethod
     def set_simulator(self):
