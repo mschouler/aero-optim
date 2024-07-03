da782b6572ff7e42018284683c712d57b1878eb5
Marc Schouler <schouler.marc@gmail.com>
2024-07-03 13:47:01 +0200

    add DLR_mesh option

--- aero_optim/mesh/cascade_mesh.py
+++ aero_optim/mesh/cascade_mesh.py
@@ -23,6 +23,7 @@ class CascadeMesh(Mesh):
         **Inner**
 
         - doutlet (float): outlet distance to the blade trailing edge.
+        - dlr_mesh (bool): builds the DLR provided mesh (True) or a simpler for adaptation (False).
         - bl_sizefar (float): boundary layer mesh size far from the curves.
         - nodes_inlet (int): the number of nodes to mesh the inlet.
         - nodes_outlet (int): the number of nodes to mesh the outlet.
@@ -36,9 +37,11 @@ class CascadeMesh(Mesh):
         - nodes_sp4 (int): the number of nodes to mesh the 3rd section of the blade suction side.
         - nodes_sp7 (int): the number of nodes to mesh the 1st section of the blade pressure side.
         - nodes_sp8 (int): the number of nodes to mesh the 2nd section of the blade pressure side.
+        - nodes_ss (int): the number of nodes to mesh the suction side (dlr_mesh set to False).
+        - nodes_ps (int): the number of nodes to mesh the pressure side (dlr_mesh set to False).
 
         Note:
-            the blade geometry is split into 9 splines (clockwise from the tip):
+            for the DLR configuration, the blade is split into 9 splines (clockwise from the tip):
 
             * 2 splines (1 and 9) for the leading edge parameterized with **le**
               i.e. each spline has **le**/2 nodes,
@@ -52,6 +55,7 @@ class CascadeMesh(Mesh):
         """
         super().__init__(config, datfile)
         self.doutlet: float = self.config["gmsh"]["domain"].get("outlet", 6.3e-2)
+        self.dlr_mesh: bool = config["gmsh"]["mesh"].get("DLR_mesh", False)
         self.bl_sizefar: float = config["gmsh"]["mesh"].get("bl_sizefar", 1e-5)
         self.nodes_inlet: int = self.config["gmsh"]["mesh"].get("nodes_inlet", 25)
         self.nodes_outlet: int = self.config["gmsh"]["mesh"].get("nodes_outlet", 17)
@@ -65,6 +69,8 @@ class CascadeMesh(Mesh):
         self.nodes_sp4: int = self.config["gmsh"]["mesh"].get("nodes_sp4", 14)
         self.nodes_sp7: int = self.config["gmsh"]["mesh"].get("nodes_sp7", 57)
         self.nodes_sp8: int = self.config["gmsh"]["mesh"].get("nodes_sp8", 32)
+        self.nodes_ss: int = self.config["gmsh"]["mesh"].get("nodes_ss", 600)
+        self.nodes_ps: int = self.config["gmsh"]["mesh"].get("nodes_ss", 600)
 
     def process_config(self):
         logger.info("processing config..")
@@ -133,25 +139,38 @@ class CascadeMesh(Mesh):
         pt_wall = [gmsh.model.geo.addPoint(p[0], p[1], p[2]) for p in wall]
 
         # blade splines and transfinite curves
-        spl_1 = gmsh.model.geo.addSpline(pt_wall[:35])
-        spl_2 = gmsh.model.geo.addSpline(pt_wall[35 - 1:88])
-        spl_3 = gmsh.model.geo.addSpline(pt_wall[88 - 1:129])
-        spl_4 = gmsh.model.geo.addSpline(pt_wall[129 - 1:157])
-        spl_5 = gmsh.model.geo.addSpline(pt_wall[157 - 1:168])
-        spl_6 = gmsh.model.geo.addSpline(pt_wall[168 - 1:179])
-        spl_7 = gmsh.model.geo.addSpline(pt_wall[179 - 1:245])
-        spl_8 = gmsh.model.geo.addSpline(pt_wall[245 - 1:287])
-        spl_9 = gmsh.model.geo.addSpline(pt_wall[287 - 1:322] + [pt_wall[0]])
-        gmsh.model.geo.mesh.setTransfiniteCurve(spl_1, self.le // 2, "Progression", 1.02)
-        gmsh.model.geo.mesh.setTransfiniteCurve(spl_2, self.nodes_sp2, "Progression", 1.03)
-        gmsh.model.geo.mesh.setTransfiniteCurve(spl_3, self.nodes_sp3, "Progression", 1)
-        gmsh.model.geo.mesh.setTransfiniteCurve(spl_4, self.nodes_sp4, "Progression", 0.94)
-        gmsh.model.geo.mesh.setTransfiniteCurve(spl_5, self.te // 2, "Progression", 0.97)
-        gmsh.model.geo.mesh.setTransfiniteCurve(spl_6, self.te // 2, "Progression", 1.025)
-        gmsh.model.geo.mesh.setTransfiniteCurve(spl_7, self.nodes_sp7, "Progression", 1.015)
-        gmsh.model.geo.mesh.setTransfiniteCurve(spl_8, self.nodes_sp8, "Progression", 0.955)
-        gmsh.model.geo.mesh.setTransfiniteCurve(spl_9, self.le // 2, "Progression", 0.9)
-        spl_list = [spl_1, spl_2, spl_3, spl_4, spl_5, spl_6, spl_7, spl_8, spl_9]
+        if self.dlr_mesh:
+            spl_1 = gmsh.model.geo.addSpline(pt_wall[:35])
+            spl_2 = gmsh.model.geo.addSpline(pt_wall[35 - 1:88])
+            spl_3 = gmsh.model.geo.addSpline(pt_wall[88 - 1:129])
+            spl_4 = gmsh.model.geo.addSpline(pt_wall[129 - 1:157])
+            spl_5 = gmsh.model.geo.addSpline(pt_wall[157 - 1:168])
+            spl_6 = gmsh.model.geo.addSpline(pt_wall[168 - 1:179])
+            spl_7 = gmsh.model.geo.addSpline(pt_wall[179 - 1:245])
+            spl_8 = gmsh.model.geo.addSpline(pt_wall[245 - 1:287])
+            spl_9 = gmsh.model.geo.addSpline(pt_wall[287 - 1:322] + [pt_wall[0]])
+            gmsh.model.geo.mesh.setTransfiniteCurve(spl_1, self.le // 2, "Progression", 1.02)
+            gmsh.model.geo.mesh.setTransfiniteCurve(spl_2, self.nodes_sp2, "Progression", 1.03)
+            gmsh.model.geo.mesh.setTransfiniteCurve(spl_3, self.nodes_sp3, "Progression", 1)
+            gmsh.model.geo.mesh.setTransfiniteCurve(spl_4, self.nodes_sp4, "Progression", 0.94)
+            gmsh.model.geo.mesh.setTransfiniteCurve(spl_5, self.te // 2, "Progression", 0.97)
+            gmsh.model.geo.mesh.setTransfiniteCurve(spl_6, self.te // 2, "Progression", 1.025)
+            gmsh.model.geo.mesh.setTransfiniteCurve(spl_7, self.nodes_sp7, "Progression", 1.015)
+            gmsh.model.geo.mesh.setTransfiniteCurve(spl_8, self.nodes_sp8, "Progression", 0.955)
+            gmsh.model.geo.mesh.setTransfiniteCurve(spl_9, self.le // 2, "Progression", 0.9)
+            spl_list = [spl_1, spl_2, spl_3, spl_4, spl_5, spl_6, spl_7, spl_8, spl_9]
+        else:
+            spl_le = gmsh.model.geo.addSpline(pt_wall[287 - 1:322] + [pt_wall[0]] + pt_wall[:35])
+            spl_ss = gmsh.model.geo.addSpline(
+                pt_wall[35 - 1:88] + pt_wall[88 - 1:129] + pt_wall[129 - 1:157]
+            )
+            spl_te = gmsh.model.geo.addSpline(pt_wall[157 - 1:168] + pt_wall[168 - 1:179])
+            spl_ps = gmsh.model.geo.addSpline(pt_wall[179 - 1:245] + pt_wall[245 - 1:287])
+            gmsh.model.geo.mesh.setTransfiniteCurve(spl_le, self.le, "Progression", 1.0)
+            gmsh.model.geo.mesh.setTransfiniteCurve(spl_ss, self.nodes_ss, "Progression", 1.0)
+            gmsh.model.geo.mesh.setTransfiniteCurve(spl_te, self.te, "Progression", 1)
+            gmsh.model.geo.mesh.setTransfiniteCurve(spl_ps, self.nodes_ps, "Progression", 1.0)
+            spl_list = [spl_le, spl_ss, spl_te, spl_ps]
         blade_loop = gmsh.model.geo.addCurveLoop(spl_list)
 
         # domain construction points
@@ -175,54 +194,63 @@ class CascadeMesh(Mesh):
         pt_340 = gmsh.model.geo.addPoint(-6e-2, -9.61e-3, 0.)
 
         # domain construction lines
-        l_10 = gmsh.model.geo.addLine(pt_340, pt_323)
-        l_11 = gmsh.model.geo.addLine(pt_323, pt_324)
-        l_12 = gmsh.model.geo.addLine(pt_339, pt_340)
-        l_13 = gmsh.model.geo.addLine(pt_324, pt_339)
-        l_14 = gmsh.model.geo.addLine(pt_324, pt_325)
-        l_15 = gmsh.model.geo.addLine(pt_325, pt_326)
-        l_16 = gmsh.model.geo.addLine(pt_326, pt_327)
-        l_17 = gmsh.model.geo.addLine(pt_327, pt_328)
-        l_18 = gmsh.model.geo.addLine(pt_328, pt_329)
-        l_19 = gmsh.model.geo.addLine(pt_329, pt_330)
-        l_20 = gmsh.model.geo.addLine(pt_330, pt_331)
-        l_21 = gmsh.model.geo.addLine(pt_331, pt_332)
-        l_22 = gmsh.model.geo.addLine(pt_332, pt_333)
-        l_23 = gmsh.model.geo.addLine(pt_333, pt_334)
-        l_24 = gmsh.model.geo.addLine(pt_334, pt_335)
-        l_25 = gmsh.model.geo.addLine(pt_335, pt_336)
-        l_26 = gmsh.model.geo.addLine(pt_336, pt_337)
-        l_27 = gmsh.model.geo.addLine(pt_337, pt_338)
-        l_28 = gmsh.model.geo.addLine(pt_338, pt_339)
+        l_10 = gmsh.model.geo.addLine(pt_340, pt_323, tag=10)
+        l_11 = gmsh.model.geo.addLine(pt_323, pt_324, tag=11)
+        l_12 = gmsh.model.geo.addLine(pt_339, pt_340, tag=12)
+        if self.dlr_mesh:
+            l_13 = gmsh.model.geo.addLine(pt_324, pt_339, tag=13)
+        l_14 = gmsh.model.geo.addLine(pt_324, pt_325, tag=14)
+        l_15 = gmsh.model.geo.addLine(pt_325, pt_326, tag=15)
+        l_16 = gmsh.model.geo.addLine(pt_326, pt_327, tag=16)
+        l_17 = gmsh.model.geo.addLine(pt_327, pt_328, tag=17)
+        l_18 = gmsh.model.geo.addLine(pt_328, pt_329, tag=18)
+        l_19 = gmsh.model.geo.addLine(pt_329, pt_330, tag=19)
+        l_20 = gmsh.model.geo.addLine(pt_330, pt_331, tag=20)
+        l_21 = gmsh.model.geo.addLine(pt_331, pt_332, tag=21)
+        l_22 = gmsh.model.geo.addLine(pt_332, pt_333, tag=22)
+        l_23 = gmsh.model.geo.addLine(pt_333, pt_334, tag=23)
+        l_24 = gmsh.model.geo.addLine(pt_334, pt_335, tag=24)
+        l_25 = gmsh.model.geo.addLine(pt_335, pt_336, tag=25)
+        l_26 = gmsh.model.geo.addLine(pt_336, pt_337, tag=26)
+        l_27 = gmsh.model.geo.addLine(pt_337, pt_338, tag=27)
+        l_28 = gmsh.model.geo.addLine(pt_338, pt_339, tag=28)
 
         # transfinite curves on non-blade boundaries
         gmsh.model.geo.mesh.setTransfiniteCurve(l_10, self.nodes_inlet, "Progression", 1.)
-        gmsh.model.geo.mesh.setTransfiniteCurve(l_13, self.nodes_inlet, "Progression", 1.)
+        if self.dlr_mesh:
+            gmsh.model.geo.mesh.setTransfiniteCurve(l_13, self.nodes_inlet, "Progression", 1.)
         gmsh.model.geo.mesh.setTransfiniteCurve(l_21, self.nodes_outlet, "Progression", 1.)
         # bottom / top periodicity
-        bottom_tags = [l_11, l_14, l_15, l_16, l_17, l_18, l_19, l_20]
-        top_tags = [l_12, l_28, l_27, l_26, l_25, l_24, l_23, l_22]
+        self.bottom_tags = [l_11, l_14, l_15, l_16, l_17, l_18, l_19, l_20]
+        self.top_tags = [l_12, l_28, l_27, l_26, l_25, l_24, l_23, l_22]
         # bottom non-curved side nodes
         gmsh.model.geo.mesh.setTransfiniteCurve(l_11, self.snodes_inlet, "Progression", 1.)
         gmsh.model.geo.mesh.setTransfiniteCurve(l_20, self.snodes_outlet, "Progression", 1.)
         # bottom curved side nodes
         _ = [gmsh.model.geo.mesh.setTransfiniteCurve(l_i, self.c_snodes, "Progression", 1.)
-             for l_i in bottom_tags[1:-1]]
+             for l_i in self.bottom_tags[1:-1]]
         # periodic boundaries /y direction
         gmsh.model.geo.synchronize()
         translation = [1, 0, 0, 0, 0, 1, 0, 0.04039, 0, 0, 1, 0, 0, 0, 0, 1]
-        for tid, bid in zip(top_tags, bottom_tags):
+        for tid, bid in zip(self.top_tags, self.bottom_tags):
             gmsh.model.mesh.setPeriodic(1, [tid], [bid], translation)
 
         # closed curve loop and computational domain surface definition
-        cloop_2 = gmsh.model.geo.addCurveLoop([l_10, l_11, l_12, l_13])
-        cloop_3 = gmsh.model.geo.addCurveLoop(
-            [-l_13, l_14, l_15, l_16, l_17, l_18, l_19, l_20,
-             l_21, l_22, l_23, l_24, l_25, l_26, l_27, l_28]
-        )
-        surf_1 = gmsh.model.geo.addPlaneSurface([cloop_2], tag=1002)
-        surf_2 = gmsh.model.geo.addPlaneSurface([cloop_3, blade_loop], tag=1003)
-        gmsh.model.geo.mesh.setTransfiniteSurface(surf_1)
+        if self.dlr_mesh:
+            cloop_2 = gmsh.model.geo.addCurveLoop([l_10, l_11, l_12, l_13])
+            cloop_3 = gmsh.model.geo.addCurveLoop(
+                [-l_13, l_14, l_15, l_16, l_17, l_18, l_19, l_20,
+                 l_21, l_22, l_23, l_24, l_25, l_26, l_27, l_28,]
+            )
+            surf_1 = gmsh.model.geo.addPlaneSurface([cloop_2], tag=1002)
+            surf_2 = gmsh.model.geo.addPlaneSurface([cloop_3, blade_loop], tag=1003)
+            gmsh.model.geo.mesh.setTransfiniteSurface(surf_1)
+        else:
+            cloop_3 = gmsh.model.geo.addCurveLoop(
+                [l_10, l_11, l_14, l_15, l_16, l_17, l_18, l_19, l_20,
+                 l_21, l_22, l_23, l_24, l_25, l_26, l_27, l_28, l_12]
+            )
+            surf_2 = gmsh.model.geo.addPlaneSurface([cloop_3, blade_loop], tag=1003)
 
         # Fields definition
         # Boundary Layer
@@ -239,7 +267,7 @@ class CascadeMesh(Mesh):
         self.build_minaniso_field([f_cyl1, f_cyl2])
 
         # define physical groups for boundary conditions
-        self.surf_tag = [surf_1, surf_2]
+        self.surf_tag = [surf_1, surf_2] if self.dlr_mesh else [surf_2]
         gmsh.model.geo.addPhysicalGroup(2, self.surf_tag, tag=100, name="field")
         gmsh.model.geo.addPhysicalGroup(1, [l_10], tag=10, name="inlet")
         logger.info(f"BC: Inlet tags are {[l_10]}")
@@ -247,11 +275,13 @@ class CascadeMesh(Mesh):
         logger.info(f"BC: Outlet tags are {[l_21]}")
         gmsh.model.geo.addPhysicalGroup(1, spl_list, tag=30, name="wall")
         logger.info(f"BC: Wall tags are {spl_list}")
-        gmsh.model.geo.addPhysicalGroup(1, top_tags, tag=40, name="uwall")
-        logger.info(f"BC: Top tags are {top_tags}")
-        gmsh.model.geo.addPhysicalGroup(1, bottom_tags, tag=50, name="lwall")
-        logger.info(f"BC: Bottom tags are {bottom_tags}")
+        gmsh.model.geo.addPhysicalGroup(1, self.top_tags, tag=40, name="uwall")
+        logger.info(f"BC: Top tags are {self.top_tags}")
+        gmsh.model.geo.addPhysicalGroup(1, self.bottom_tags, tag=50, name="lwall")
+        logger.info(f"BC: Bottom tags are {self.bottom_tags}")
 
-        # non-corner points defined as flow-field and inner block line nodes
-        self.non_corner_tag.extend([abs(s_tag) for s_tag in self.surf_tag])
-        self.non_corner_tag.append(abs(l_13))
+        # non-corner points defined as flow-field, inner block line and wall nodes
+        self.non_corner_tags.extend([abs(s_tag) for s_tag in self.surf_tag])
+        self.non_corner_tags.extend([abs(s_tag) for s_tag in spl_list])
+        if self.dlr_mesh:
+            self.non_corner_tags.append(abs(l_13))
