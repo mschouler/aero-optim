8512ee62e4577a4a974317d827684d93199c3def
Marc Schouler <schouler.marc@gmail.com>
2025-04-18 10:17:23 +0200

    fix bug in QoI_convergence

--- examples/LRN-CASCADE/cascade_musicaa_base/execute.py
+++ examples/LRN-CASCADE/cascade_musicaa_base/execute.py
@@ -433,7 +433,7 @@ def QoI_convergence(sim_outdir: str,
     else:
         delta_1 = abs((QoIs[-1] - QoIs[-2]) / QoIs[-1])
         delta_2 = abs((QoIs[-1] - QoIs[-3]) / QoIs[-1])
-        order = -np.log10(max(delta_1, delta_2))
+        order = -np.log10(max(delta_1, delta_2))[0]
         print(f"QoI convergence order = {order:.2f}")
         return order < QoIs_convergence_order
 
