fix scaling issue and base classes arg passing

aero_optim/ffd/ffd.py
@@ -86,7 +86,7 @@ class FFD_2D(Deform):
     """
     def __init__(
             self, dat_file: str, ncontrol: int,
-            pad: tuple[int, int] = (1, 1), header: int = 2, **kwargs
+            pad: tuple[int, int] = (1, 1), **kwargs
     ):
         """
         Instantiates the FFD_2D object.
@@ -96,7 +96,6 @@ class FFD_2D(Deform):
         - dat_file (str): path to input_geometry.dat.
         - ncontrol (int): the number of control points on each side of the lattice.
         - pad (tuple[int, int]): padding around the displacement vector.
-        - header (int): the number of header lines in dat_file.
 
         **Inner**
 
@@ -110,7 +109,7 @@ class FFD_2D(Deform):
         - M (int): the number of control points in the y direction of each side of the lattice.
         - lat_pts (np.ndarray): the geometry coordinates in the lattice referential.
         """
-        super().__init__(dat_file, ncontrol, header)
+        super().__init__(dat_file, ncontrol, **kwargs)
         assert pad in [(0, 0), (1, 1), (0, 1), (1, 0)], f"wrong padding: {pad}"
         self.pad: tuple[int, int] = pad
         self.L: int = ncontrol - 1 + sum(pad)